3001aa22332ab235db5706b6a4657c1b09dd7574
Aman <aman.pr.rel@gmail.com>
2023-03-10 12:50:35 +0530

    implement __get_decks() to get name of the decks
    `__get_decks(deliminator: str, file_paths: list) -> list` will help user to get the name of all the decks from all the file user has provided

--- main.py
+++ main.py
@@ -14,7 +14,17 @@ def show_custom_dialog() -> dict:
 
 def create_multiple_decks() -> None:
     # TODO while logging the info the dict of info is printing two times. Perhaps something is running multiple times fit it
-    my_logger.add_log(show_custom_dialog())
+    info = show_custom_dialog()
+    my_logger.add_log(info)
+    info = __get_decks(info["deliminator"], info["files"])
+    my_logger.add_log(info, my_logger.logging.DEBUG)
+
+    if __create_decks(info):
+        my_logger.add_log("Decks created successfully")
+    else:
+        my_logger.add_log("Error occurred in creating decks",
+                          my_logger.logging.DEBUG)
+
 
 
 def __create_decks(decks: list) -> bool:
@@ -27,7 +37,11 @@ def __create_decks(decks: list) -> bool:
 def __get_decks(deliminator: str, file_paths: list) -> list:
     """Create list of the decks from the files path and deliminator provided
     Returns list of the decks"""
-    return [None, None]
+    decks = set()
+    for file_path in file_paths:
+        with open(file_path, 'r') as f:
+            decks.update(f.read().split(deliminator))
+    return [item for item in list(decks) if item.strip()]
 
 action = QAction("Create Multiple Decks", mw)
 qconnect(action.triggered, create_multiple_decks)
